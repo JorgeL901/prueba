7f9baef2886a885a52a3770b023bd511f2553975
Jorge Ivan Lopez Leon <jorgeill.901@gmail.com>
2022-08-18 18:52:22 -0500

    Primer ejercicio suma express

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/prueba',function(req,res){
     res.sendFile(__dirname+"/index.html")// send o sendfile envia algo como res 
 });
+app.post('/formulario',function(req,res){
+    var n1=req.body.n1;
+    var n2=req.body.n2;
+    var resultado=parseInt(n1)+parseInt(n2)
+    res.send(n1+"+"+n2+"="+resultado)
+    
+})
 //3 Listen
 app.listen(4000,function(){
     console.log("Servidor iniciado")
